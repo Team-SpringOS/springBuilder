4b0a6d52571fec69571d71df187cac27c186cc00
SpringOS <126188458+Team-SpringOS@users.noreply.github.com>
2023-02-25 11:29:24 +0530

    First Draft !!...

---
+++ install.sh
@@ -0,0 +1,39 @@
+# Install Desktop Environment
+
+sudo nala install -y vanilla-gnome-desktop vanilla-gnome-default-settings
+
+# Disable Wayland
+
+sudo sed -i "s/#WaylandEnable=false/WaylandEnable=false/g" /etc/gdm3/custom.conf
+
+# Debloating Vanilla-Gnome
+
+sudo nala remove --purge -y \
+ubuntu-mono \
+ubuntu-gnome-wallpapers* \
+gnome-console \
+libreoffice* \
+gnome-online-accounts \
+gnome-user-docs \
+gnome-initial-setup \
+simple-scan \
+transmission* \
+baobab \
+gnome-text-editor \
+yelp \
+seahorse \
+aisleriot \
+cheese \
+gnome-mahjongg \
+gnome-mines \
+rhythmbox* \
+gnome-initial-setup \
+gnome-user-docs \
+gnome-sudoku \
+
+# Flatpak Setup
+
+sduo nala install -y flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+sudo reboot
