6baba4ca7d0fd0564687a5fb0598d3392c64a28f
SpringOS <126188458+Team-SpringOS@users.noreply.github.com>
2023-03-15 07:44:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 sudo apt install -y vanilla-gnome-desktop vanilla-gnome-default-settings gedit ubuntu-mono- ubuntu-gnome-wallpapers*- gnome-console- libreoffice*- \
-gnome-online-accounts- gnome-user-docs- gnome-initial-setup- simple-scan- transmission*- baobab- gnome-text-editor- yelp- seahorse- aisleriot- cheese- \
+simple-scan- transmission*- baobab- gnome-text-editor- seahorse- aisleriot- cheese- \
 gnome-mahjongg- gnome-mines- rhythmbox*- gnome-sudoku- dconf-editor- deja-dup- gnome-startup-applications- usb-creator*- gnome-shell-extension-prefs- 
