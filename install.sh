2e2342983109e6b885b7415085f5b1a985429d15
SpringOS <126188458+Team-SpringOS@users.noreply.github.com>
2023-03-15 17:45:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
-sudo apt install -y vanilla-gnome-desktop vanilla-gnome-default-settings gedit ubuntu-mono- ubuntu-gnome-wallpapers*- gnome-console- libreoffice*- \
-simple-scan- transmission*- baobab- gnome-text-editor- seahorse- aisleriot- cheese- \
-gnome-mahjongg- gnome-mines- rhythmbox*- gnome-sudoku- deja-dup- gnome-startup-applications- usb-creator*- gnome-shell-extension-prefs- 
+sudo apt install -y vanilla-gnome-desktop vanilla-gnome-default-settings gedit \
+aisleriot- baobab- brltty- cheese- deja-dup- firefox fonts-indic- fonts-kacst-one- fonts-khmeros-core- fonts-lao- \
+fonts-liberation- fonts-lklug-sinhala- fonts-noto-cjk- fonts-sil-abyssinica- fonts-sil-padauk- fonts-thai-tlwg- \
+fonts-tibetan-machine- fonts-ubuntu- fwupd- fwupd-signed- gnome-accessibility-themes- gnome-calendar- gnome-characters- \
+gnome-contacts- gnome-disk-utility- gnome-initial-setup- gnome-keyring- gnome-mahjongg- gnome-maps- gnome-mines- \
+gnome-photos- gnome-sudoku- gnome-sushi- gnome-text-editor- gnome-weather- gvfs-fuse- libreoffice-calc- libreoffice-gnome- \
+libreoffice-impress- libreoffice-math- libreoffice-ogltrans- libreoffice-pdfimport- libreoffice-style-tango- \
+libreoffice-writer- mousetweaks- rhythmbox- seahorse- simple-scan- snapd- speech-dispatcher- totem- transmission-gtk- \
+ubuntu-gnome-wallpapers- usb-creator-gtk
